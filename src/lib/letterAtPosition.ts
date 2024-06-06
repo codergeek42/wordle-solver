462fd24fd2eb762fd762866524ca668a9b3cc5cf
Peter Gordon <codergeek42@gmail.com>
2024-06-05 22:43:59 -0700

    feat: initial letterAtPosition implementation

---
+++ src/lib/letterAtPosition.ts
@@ -0,0 +1,33 @@
+import { omit } from 'lodash';
+
+export enum LetterAtPosition {
+    Impossible,
+    Possible,
+    Mandatory
+}
+export type LetterRule = {
+    letter: string;
+    required: LetterAtPosition;
+};
+
+export type LetterAtPositionRule = LetterRule & {
+    position: number;
+};
+
+export function doesLetterMatchRule(letter: string, rule: LetterRule): boolean {
+    switch (rule.required) {
+        case LetterAtPosition.Impossible:
+            return letter != rule.letter;
+        case LetterAtPosition.Possible:
+            return true;
+        case LetterAtPosition.Mandatory:
+            return letter == rule.letter;
+    }
+}
+
+export function doesWordMatchLetterAtPosition(
+    word: string,
+    rule: LetterAtPositionRule
+) {
+    return doesLetterMatchRule(word[rule.position], omit(rule, 'position'));
+}
