fce8d035ca4891fdeab70b33bac08d81b9cb1042
Peter Gordon <codergeek42@gmail.com>
2024-09-02 13:22:37 -0700

    style: make consistent-type-definitions a warning instead of error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,5 +6,14 @@ export default [
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
-    ...tseslint.configs.stylistic
+    ...tseslint.configs.stylistic,
+    {
+        rules: {
+            // If I want the openness of an interface instead of a type for object definitions,
+            // then I'll make the thing an interface. Otherwise this recommendation is silly IMHO. 
+            // Make it a warning, so it's still visible just in case; but that it doesn't block CI.
+            "@typescript-eslint/consistent-type-definitions": "warn"
+        }
+    }
+    
 ];
