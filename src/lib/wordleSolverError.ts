af55cc645f4f37ad2ac1324ddba45ac31bae8428
Peter Gordon <codergeek42@gmail.com>
2024-10-12 21:36:18 -0700

    fix: WordleSolverTestError extends

--- src/lib/wordleSolverError.ts
+++ src/lib/wordleSolverError.ts
@@ -4,4 +4,4 @@ export class MissingPositionError extends WordleSolverError {}
 
 export class NoMoreGuessesError extends WordleSolverError {}
 
-export class WordleSolverTestError extends Error {}
+export class WordleSolverTestError extends WordleSolverError {}
