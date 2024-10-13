a22cce93c2ac4f29e1c315f5a225ba2d71eb6cbe
Peter Gordon <codergeek42@gmail.com>
2024-10-12 21:38:18 -0700

    test: ignore integration tests for now (will be follow-up)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: true,
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/__tests__/integration/'],
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts', '__data__/**/*.ts'],
     coverageReporters: ['text'],
