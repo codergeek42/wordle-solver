58bb4b71a630dbd2450dd710b5754c1e04da937c
Peter Gordon <codergeek42@gmail.com>
2024-10-13 18:13:07 -0700

    fix: PR feedback
    Signed-off-by: Peter Gordon <codergeek42@gmail.com>

--- src/lib/guesserStrategies/letterFrequencyStrategy.ts
+++ src/lib/guesserStrategies/letterFrequencyStrategy.ts
@@ -7,14 +7,6 @@ export default class LetterFrequencyStrategy extends NextWordGuesserStrategyBase
     }
 
     scoreForGuess(guess: string): number {
-        // const previousLetterFrequencies = countBy(this.wordList.words.join(''));
-        // const totalLettersCount = sum(values(previousLetterFrequencies));
-        // // console.log('@@ '.concat(JSON.stringify({ totalLettersCount, previousLetterFrequencies }, null, 1)));
-        // const guessScore = sum(
-        //     values(mapValues(countBy(guess), (count, letter) => count * previousLetterFrequencies[letter]))
-        // );
-        // return guessScore / totalLettersCount;
-
         const countLettersAtPosition = (position: number): Record<string, number> =>
             countBy(this.wordList.words.flatMap((word) => [word[position]]));
 
