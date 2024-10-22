f957450a4ff35c582a9fd62a0bd6f60746b51e81
Peter Gordon <codergeek42@gmail.com>
2024-10-21 22:39:07 -0700

    docs: add tsdoc for letterAtPosition

--- src/lib/letterAtPosition.ts
+++ src/lib/letterAtPosition.ts
@@ -18,6 +18,9 @@
  * see <https://www.gnu.org/licenses/gpl-3.0.html>.
  */
 
+/**
+ * Stores the requirement for a given letter at some position in the word.
+ */
 export enum LetterAtPositionInWord {
     // Right letter, right position.
     Mandatory = 'Mandatory',
@@ -27,6 +30,21 @@ export enum LetterAtPositionInWord {
     Impossible = 'Impossible'
 }
 
+/**
+ * A comparator for sorting two `LetterAtPositionInWordRule` objects, following the usual
+ * pattern of positive (if the first rule is greater), positive (if the second rule is
+ * greater), or zero (if they are equal).
+ *
+ * In the cae of `LetterAtPositionInWordRule` objects, this comparison is based on which
+ * of the given rules has their `required` property set to `Mandatory`: If one of the rules
+ * has a `Mandatory` and the other does not, then the one which does is the considered the
+ * greater of the two; and if neither or both of them have a `Mandatory`, then they are
+ * considered equal.
+ *
+ * @param posRuleA - The first rule.
+ * @param posRuleB - The second rule.
+ * @returns +1 if `posRuleA` > `posRuleB`; -1 if `posRuleA` < `posRuleB`; or 0 otherwise.
+ */
 export function letterAtPositionInWordRuleComparator(
     posRuleA: LetterAtPositionInWordRule,
     posRuleB: LetterAtPositionInWordRule
@@ -37,13 +55,26 @@ export function letterAtPositionInWordRuleComparator(
     return posRuleB.required == LetterAtPositionInWord.Mandatory ? -1 : 0;
 }
 
+/**
+ * A combination of letter and its associated `LetterAtPositionInWord` requirement.
+ * @see {@link LetterAtPositionInWord}
+ */
 export type LetterRule = {
     letter: string;
     required: LetterAtPositionInWord;
 };
 
+/**
+ * A combination of a given `LetterRule` with its associated position.
+ * @see {@link LetterRule}
+ */
 export type LetterAtPositionInWordRule = LetterRule & {
     position?: number;
 };
 
+/**
+ * The same type as `LetterAtPositionInWordRule`, but with the the `position` property
+ * not optional.
+ * @see {@link LetterAtPositionInWordRule}
+ */
 export type LetterWithPosition = Required<Omit<LetterAtPositionInWordRule, 'required'>>;
