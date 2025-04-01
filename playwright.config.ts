d1dad190222204fa80f0f1281e62143e078533fe
Peter Gordon <codergeek42@gmail.com>
2025-03-31 21:03:04 -0700

    chore: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
     // TODO: For >6 attempts, cache guesses + result in file and use Playwright retries to get answer.
     retries: 0,
 
-    reporter: 'json',
+    // TODO: use parallel runners for guesser algorithm comparison?
+    fullyParallel: false,
+    workers: 1,
+
+    reporter: 'list',
 
     use: {
         baseURL: 'https://www.nytimes.com/games/wordle',
