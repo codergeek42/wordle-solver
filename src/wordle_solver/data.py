f60ccf5be3fb28ee636cb1b19d982397652aeeb6
Peter Gordon <codergeek42@gmail.com>
2025-06-28 10:16:25 -0700

    ci: initial Python migration

---
+++ src/wordle_solver/data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3 -tt
+#####
+# wordle_solver: A clever algorithm and automated tool to solve the
+# 	NYTimes daily Wordle puzzle game.
+# Copyright (C) 2023 Peter Gordon <codergeek42@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program, namely the "LICENSE" text file.  If not,
+# see <https://www.gnu.org/licenses/gpl-3.0.html>.
+#####
+
+import string
+
+Alphabet = string.ascii_uppercase
+
+DisclaimerOfWarranty = '''\
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER
+PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO
+THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE
+DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+'''
+
+CopyleftInformation = '''\
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program (the included LICENSE text file). If not, see
+<https://www.gnu.org/licenses/> .
+'''
+
+WelcomeBanner = '''
+This program comes with ABSOLUTELY NO WARRANTY; for details, select `(No)
+Warranty Information` from the main menu. This is free software, and you are
+welcome to redistribute it under certain conditions; select `Copyleft
+Information` from the main menu for details.
+'''
